dde4866a88e3e0effb6a1adb6edeaa0dc6581730
Nina Olear <pninak@web.de>
2022-03-28 19:34:32 +0200

    support mariadb

---
+++ pkg/database/mariadb/mariadb.go
@@ -0,0 +1,125 @@
+package mariadb
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+
+	"github.com/MninaTB/vacadm/pkg/model"
+)
+
+const (
+	basicUserSelect = `
+	  	SELECT
+			uuid,
+		  	parent_uuid,
+		  	created_at, updated_at,
+		  	firstname, lastname,
+		  	email
+	  	FROM user
+	`
+
+	userSelectByID = basicUserSelect + `
+	  	WHERE uuid = :uuid
+	`
+
+	userCreate = `
+	 	INSERT INTO user (
+		  	uuid,
+			parent_uuid,
+			fistname, 
+			lastname,
+		  	email,
+		  	created_at, updated_at
+		)
+	  VALUES (
+		 	:uuid, :parent_uuid,
+		  	:firstname, :lastname,
+		  	email,
+			DEFAULT, DEFAULT
+	  )
+	`
+
+	userUpdate = `
+	  	UPDATE user 
+		SET (
+			uuid :uuid, parent_uuid :parent_uuid, 
+			firstname :firstname, lastname :lastname, 
+			email :email
+		)
+		WHERE uuid = :uuid
+	`
+
+	userDelete = `
+		DELETE FROM user
+		WHERE uuid = :uuid
+	`
+)
+
+func NewMariaDB(db *sql.DB) *MariaDB {
+	return &MariaDB{
+		db:     db,
+		logger: logrus.New().WithField("component", "mariaDB"),
+	}
+}
+
+type MariaDB struct {
+	db     *sql.DB
+	logger logrus.FieldLogger
+}
+
+func (m *MariaDB) CreateUser(u *model.User) (*model.User, error) {
+	u.ID = uuid.NewString()
+	_, err := m.db.ExecContext(context.Background(), userCreate, u.ID, u.FirstName, u.LastName, u.Email, u.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return m.GetUserByID(u.ID)
+}
+
+func (m *MariaDB) GetUserByID(uuid string) (*model.User, error) {
+	row := m.db.QueryRowContext(context.Background(), userSelectByID, uuid)
+	err := row.Err()
+	if err != nil {
+		return nil, err
+	}
+	u := &model.User{}
+	row.Scan(u.ID, u.ParentID, u.CreatedAt, u.UpdatedAt, u.FirstName, u.LastName, u.Email)
+	return u, nil
+}
+
+// ?
+func (m *MariaDB) ListUsers() (*model.User, error) {
+	row := m.db.QueryRowContext(context.Background(), userSelectByID)
+	err := row.Err()
+	if err != nil {
+		return nil, err
+	}
+	u := &model.User{}
+	row.Scan(u.ID, u.ParentID, u.CreatedAt, u.UpdatedAt, u.FirstName, u.LastName, u.Email)
+	return u, nil
+}
+
+// ?
+func (m *MariaDB) UpdateUser(u *model.User) (*model.User, error) {
+	row := m.db.QueryRowContext(context.Background(), userUpdate)
+	err := row.Err()
+	if err != nil {
+		return nil, err
+	}
+	user := &model.User{}
+	row.Scan(user.ID, user.ParentID, user.CreatedAt, user.UpdatedAt, user.FirstName, user.LastName, user.Email)
+	return user, nil
+}
+
+// ?
+func (m *MariaDB) DeleteUser(uuid string) error {
+	row := m.db.QueryRowContext(context.Background(), userDelete, uuid)
+	err := row.Err()
+	if err != nil {
+		return err
+	}
+	return err
+}
---
+++ pkg/database/mariadb/migrations/00001.sql
@@ -0,0 +1,60 @@
+CREATE TABLE team (
+    id UUID NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    created_at DATE NOT NULL,
+    deleted_at DATE,
+    updated_at DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE user (
+    id UUID NOT NULL,
+    parent_id UUID,
+    team_id UUID NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATE NOT NULL,
+    deleted_at DATE,
+    updated_at DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(team_id) REFERENCES team(id)
+);
+
+CREATE TABLE vaccation (
+    id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    approved_by UUID,
+    `from` DATE NOT NULL,
+    `to` DATE NOT NULL,
+    created_at DATE NOT NULL,
+    deleted_at DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES user(id),
+    FOREIGN KEY(approved_id) REFERENCES user(id)
+)
+
+CREATE TABLE vaccation_request (
+    id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    `from` DATE NOT NULL,
+    `to` DATE NOT NULL,
+    created_at DATE NOT NULL,
+    deleted_at DATE,
+    updated_at DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES user(id),
+)
+
+CREATE TABLE vaccation_ressource (
+    id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    yearlyDays INT NOT NULL,
+    `from` DATE NOT NULL,
+    `to` DATE NOT NULL,
+    created_at DATE NOT NULL,
+    deleted_at DATE,
+    updated_at DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES user(id),
+)
